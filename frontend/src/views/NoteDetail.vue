fc6ec1d505a8ec94cc623d9879594033c758c4eb
tongyu-web <2825919095@qq.com>
2026-06-19 14:26:30 +0800

    fix: SVG fill not cascading to path elements - use inline fill/stroke bindings

--- frontend/src/views/NoteDetail.vue
+++ frontend/src/views/NoteDetail.vue
@@ -113,19 +113,19 @@
                     </div>
                     <div class="bottom-icons">
                       <div class="bottom-icon-item" @click="toggleLike">
-                        <svg :class="note.is_liked ? 'like-active' : ''" viewBox="0 0 24 24" width="20" height="20"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
+                        <svg viewBox="0 0 24 24" width="20" height="20" :fill="note.is_liked ? '#ff2442' : 'none'" :stroke="note.is_liked ? '#ff2442' : '#666'" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
                         <span>{{ note.like_count || 0 }}</span>
                       </div>
                       <div class="bottom-icon-item" @click="showFolderDialog">
-                        <svg :class="note.is_favorited ? 'fav-active' : ''" viewBox="0 0 24 24" width="20" height="20"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/></svg>
+                        <svg viewBox="0 0 24 24" width="20" height="20" :fill="note.is_favorited ? '#ffb800' : 'none'" :stroke="note.is_favorited ? '#ffb800' : '#666'" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/></svg>
                         <span>{{ note.fav_count || 0 }}</span>
                       </div>
                       <div class="bottom-icon-item" @click="toggleCommentCompose">
-                        <svg viewBox="0 0 24 24" width="20" height="20"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"/></svg>
+                        <svg viewBox="0 0 24 24" width="20" height="20" fill="none" stroke="#666" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"/></svg>
                         <span>{{ note.comment_count || 0 }}</span>
                       </div>
                       <div class="bottom-icon-item">
-                        <svg viewBox="0 0 24 24" width="20" height="20"><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"/><polyline points="16 6 12 2 8 6"/><line x1="12" y1="2" x2="12" y2="15"/></svg>
+                        <svg viewBox="0 0 24 24" width="20" height="20" fill="none" stroke="#666" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"/><polyline points="16 6 12 2 8 6"/><line x1="12" y1="2" x2="12" y2="15"/></svg>
                       </div>
                     </div>
                   </div>
@@ -724,19 +724,7 @@ async function submitComment() {
   color: #ff2442;
 }
 .bottom-icon-item svg {
-  fill: none;
-  stroke: currentColor;
-  stroke-width: 2;
-  stroke-linecap: round;
-  stroke-linejoin: round;
-}
-.bottom-icon-item .like-active {
-  fill: #ff2442;
-  stroke: #ff2442;
-}
-.bottom-icon-item .fav-active {
-  fill: #ffb800;
-  stroke: #ffb800;
+  transition: fill 0.15s, stroke 0.15s;
 }
 
 /* Composing panel */
